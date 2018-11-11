b18f968090fdaf65bde90dc58dbbd07ee1c013c7
Tatsiana Gelahova <tgelahova99@gmail.com>
2018-11-11 22:07:59 +0300

    Add Search element

---
+++ src/js/elements/Search.js
@@ -0,0 +1,44 @@
+import { Request } from "../Request";
+import { EVERYTHING } from "../constants/request";
+import { ShowResults } from "./SearchResults";
+
+class Search {
+  constructor() {
+    this._searchInput = document.getElementById("search-input");
+    this._searchButton = document.getElementById("search-btn");
+    this._searchResults = document.getElementById('search-results');
+
+    this.getNews = this.getNews.bind(this);
+    this._searchButton.addEventListener("click", this.getNews);
+  }
+
+  //show babel settings to allow arrow functions
+  getNews(e) {
+    const inputText = this._searchInput.value;
+    console.log("input value", inputText);
+
+    new Request(EVERYTHING, { q: inputText })
+      .send()
+      .then(ShowResults.render)
+  }
+
+  render(articles) {
+    const innerHTML = articles.reduce((acc, article) => acc + `
+      <li class="search-results">
+        <div class="article-content">
+          <a href='${article.url}' target="_blank">${article.title}</a>
+          <p>${article.description}</p>
+          <p>${article.content}</p>
+          <p>${article.publishedAt}</p>
+        </div>
+        <div class="article-image">
+          <img src=${article.urlToImage}/>
+        </div>
+      </li>
+    `, ``);
+
+    SearchResults.node.insertAdjacentHTML('beforeend', `<ul class="top-headlines-container">${innerHTML}</ul>`);
+  }
+}
+
+export default Search;
