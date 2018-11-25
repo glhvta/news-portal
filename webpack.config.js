55f9dfb93ba5dd079e285158771b5d1f90fc2e25
Tatsiana Gelahova <tgelahova99@gmail.com>
2018-11-25 18:09:15 +0300

    Add HtmlWebpackPlugin and CleanWebpackPlugin plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require("path");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
   entry: ["./polyfills", "./src/js/index.js"],
@@ -16,26 +18,26 @@ module.exports = {
           loader: "babel-loader",
           options: {
             presets: ["@babel/preset-env"],
-            plugins: [
-              "@babel/plugin-proposal-class-properties"
-            ]
+            plugins: ["@babel/plugin-proposal-class-properties"]
           }
         }
       },
       {
         test: /\.css$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          "css-loader"
-        ]
+        use: [MiniCssExtractPlugin.loader, "css-loader"]
       }
     ]
   },
   devServer: {
     port: 8000,
-    publicPath: '/dist/'
-    
+    publicPath: "/dist/"
   },
-  plugins: [new MiniCssExtractPlugin({ filename: "[name].css" })],
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new MiniCssExtractPlugin({ filename: "[name].css" }),
+    new HtmlWebpackPlugin({
+      template: 'index.html'
+    })
+  ],
   devtool: "inline-source-map"
 };
